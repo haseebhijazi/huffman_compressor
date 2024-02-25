2774a3eb9453fa39631a3049f8607fbec392870e
HaseebH-Khan <haseebk841@gmail.com>
2024-02-25 17:25:33 +0530

    Completed Basic

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,49 @@ impl<T: Debug> HuffmanNode<T> {
 
 }
 
+pub fn set_codes(root: &HuffmanNode<(char, i32)>, s: String, map: &mut HashMap<char, String>) {
+    
+    if root.left.is_none() && root.right.is_none() && root.value.0.is_alphanumeric() {
+        map.insert(root.value.0, s);
+        return;
+    }
+    if let Some(node) = &root.left {
+        let mut new_s = s.clone();
+        new_s.push('0');
+        set_codes(node, new_s, map);
+    }
+    if let Some(node) = &root.right {
+        let mut new_s = s.clone();
+        new_s.push('1');
+        set_codes(node, new_s, map);
+    }
+    
+}
+
+fn print_codes(codemap: HashMap<char, String>) {
+    println!("Hamming Codes: ");
+    for (key, value) in &codemap {
+        println!("{}: {}", key, value);
+    }
+}
+
+fn calculate_stats(text: String, codemap: HashMap<char, String>, charachter_map: HashMap<char, i32>) {
+    let length = text.len();
+    println!("Bytes used by the text: {:?}", length*8);
+    let mut new_length = 0;
+    for (key, value) in &charachter_map {
+        let code = codemap.get(key);
+        let code_length: i32;
+        if let Some(c) = code {
+            code_length = c.len() as i32;
+        } else {
+            code_length = 0;
+        }
+        new_length += code_length*(*value);
+    }  
+    println!("Bytes used by after compression: {:?}", new_length);
+}
+
 fn main() {
     let mut text = String::new();
 
@@ -78,7 +121,9 @@ fn main() {
     let mut charachter_map: HashMap<char, i32> = HashMap::new();
 
     for ch in text.chars() {
-        charachter_map.entry(ch).and_modify(|counter| *counter += 1).or_insert(1);
+        charachter_map.entry(ch)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
     }
 
     let mut pq: DoublePriorityQueue<HuffmanNode<(char, i32)>, i32> = DoublePriorityQueue::new();
@@ -101,7 +146,7 @@ fn main() {
                 root = fnode.clone();
                 pq.push(fnode, freq1+freq2);
         } else {
-            println!("Error while retrieving node from queue");
+            println!();
         }
 
         
@@ -109,4 +154,10 @@ fn main() {
     println!("The Huffman Tree: ");
     root.clone().print_tree();
     println!();
+
+    let mut codemap: HashMap<char, String> = HashMap::new();
+    set_codes(&root, "".to_string(), &mut codemap);
+    print_codes(codemap.clone());
+    
+    calculate_stats(text, codemap.clone(), charachter_map.clone());
 }
